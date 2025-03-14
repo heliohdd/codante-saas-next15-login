e76e4df3d06d46a603a0d3344f2a40b92c07a029
Helio Dourado <heliohdd@yahoo.com.br>
2025-03-14 17:29:10 -0300

    Configurando login com email e senha no NextAuth

--- auth.ts
+++ auth.ts
@@ -1,5 +1,25 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        console.log(credentials);
+
+        // lógica de autenticação
+        // se não autenticado retorna null
+        // se autenticado retorna user
+        return {
+          name: "Hélio",
+          email: "helio@teste.com",
+          password: "123456",
+        };
+      },
+    }),
+  ],
 });
