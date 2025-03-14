35792f7ed06f07359b0c2c8d141c282d4aa0e43e
Helio Dourado <heliohdd@yahoo.com.br>
2025-03-14 20:38:16 -0300

    Tratando erros de login com useActionState()

--- app/(auth)/login/login-form.tsx
+++ app/(auth)/login/login-form.tsx
@@ -5,23 +5,38 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Form from "next/form";
 import loginAction from "./loginAction";
+import { useActionState } from "react";
 
 export default function LoginForm() {
+  const [state, formAction, isPending] = useActionState(loginAction, null);
+
   return (
-    <Form action={loginAction}>
-      <div>
-        <Label>Email</Label>
-        <Input type="email" name="email" placeholder="eu@exemplo.com" />
-      </div>
-      <div>
-        <Label>Senha</Label>
-        <Input type="password" name="password" placeholder="********" />
-      </div>
-      <div>
-        <Button className="w-full mt-6" type="submit">
-          Login
-        </Button>
-      </div>
-    </Form>
+    <>
+      {state?.success === false && (
+        <div
+          className="text-xs mb-6 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          role="alert"
+        >
+          <strong className="font-bold">Erro!</strong>
+          <br />
+          <span className="block sm:inline">{state?.message}</span>
+        </div>
+      )}
+      <Form action={formAction}>
+        <div>
+          <Label>Email</Label>
+          <Input type="email" name="email" placeholder="eu@exemplo.com" />
+        </div>
+        <div>
+          <Label>Senha</Label>
+          <Input type="password" name="password" placeholder="********" />
+        </div>
+        <div>
+          <Button className="w-full mt-6" type="submit">
+            Login
+          </Button>
+        </div>
+      </Form>
+    </>
   );
 }
--- app/(auth)/login/loginAction.ts
+++ app/(auth)/login/loginAction.ts
@@ -1,7 +1,22 @@
 "use server";
 
+import { CredentialsSignin } from "./../../../node_modules/@auth/core/src/errors";
+
 import { signIn } from "@/auth";
 
-export default async function loginAction(formData: FormData) {
-  await signIn("credentials", formData);
+export default async function loginAction(_prevState: any, formData: FormData) {
+  try {
+    await signIn("credentials", {
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
+      redirect: false,
+    });
+    return { success: true };
+  } catch (e) {
+    if (e.type === "CredentialsSignin") {
+      return { success: false, message: "Dados de login incorretors." };
+    }
+
+    return { success: false, message: "Ops, algum erro aconteceu!" };
+  }
 }
