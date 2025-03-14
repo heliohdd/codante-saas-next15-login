87d6f8aa765c939805a372fc38539b1675363d4e
Helio Dourado <heliohdd@yahoo.com.br>
2025-03-14 15:57:47 -0300

    Criação de novos usuários e tratamento de erros com useActionState()

---
+++ app/(auth)/cadastro/registerAction.ts
@@ -0,0 +1,50 @@
+"use server";
+
+import db from "@/lib/db";
+import { hashSync } from "bcrypt-ts";
+
+export default async function registerAction(
+  _prevState: any,
+  formData: FormData
+) {
+  const entries = Array.from(formData.entries());
+  const data = Object.fromEntries(entries) as {
+    email: string;
+    name: string;
+    password: string;
+  };
+
+  console.log("==== Server Action User ====");
+  console.log(data);
+
+  //   Se não tiver nome, email, senha retorna erro
+  if (!data.name || !data.email || !data.password) {
+    return {
+      message: "Preencha todos os campos",
+      success: false,
+    };
+  }
+
+  //   Se um usuário já existe
+  const user = await db.user.findUnique({ where: { email: data.email } });
+
+  if (user) {
+    return {
+      message: "Este usuário já existe",
+      success: false,
+    };
+  }
+  //   Se não existir cria o usuário
+  await db.user.create({
+    data: {
+      email: data.email,
+      name: data.name,
+      password: hashSync(data.password),
+    },
+  });
+
+  return {
+    message: "Usuário criado com sucesso",
+    success: true,
+  };
+}
